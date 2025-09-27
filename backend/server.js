251db66066121fac766f7d7abdb633b22878eb75
Benjamin Harvey <benjaminharvey2000@gmail.com>
2025-09-28 09:24:10 +0930

    Deployment ready

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,27 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
 import mongoose from "mongoose";
+import path from "path";
 import productRoutes from "./routes/product.route.js"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(express.json());
+app.use(express.json()); // Allows us to accept JSON in request body
+
+// Deployment
+const __dirname = path.resolve();
 
 app.use("/api/products", productRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
 
 app.listen(PORT, () => {
     connectDB();
